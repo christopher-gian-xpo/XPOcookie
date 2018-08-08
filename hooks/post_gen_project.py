bba20267bafd925cbccbc95ad4785a2fdf743a9a
Chris <chris@MacBook-Pro-5.local>
2018-08-08 00:47:30 -0700

    Add conda. Simplified setup steps

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,7 @@
 import subprocess
-
-subprocess.call(['pip', 'install','virtualenv'])
-subprocess.call(['virtualenv','env'])
-
-
+subprocess.call(["conda", 
+"create","--name","{{cookiecutter.directory_name}}",
+"python={{cookiecutter.python_version}}","--file","requirements.txt"])
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
--- {{cookiecutter.directory_name}}/setup_env.sh
+++ {{cookiecutter.directory_name}}/setup_env.sh
@@ -1,3 +1,2 @@
-source env/scripts/activate
 python -m ipykernel install --user --name {{cookiecutter.directory_name}}
-pip install -r requirements.txt
+
